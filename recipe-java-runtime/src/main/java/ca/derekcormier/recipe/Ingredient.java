8c7f013c8421b8c8a4f75299d0cc58d8b8fd22c5
Derek Cormier <derek.cormier@allocadia.com>
2017-12-04 16:38:45 -0800

    Push ingredient parameters to a map for serialization

--- recipe-java-runtime/src/main/java/ca/derekcormier/recipe/Ingredient.java
+++ recipe-java-runtime/src/main/java/ca/derekcormier/recipe/Ingredient.java
@@ -1,5 +1,30 @@
 package ca.derekcormier.recipe;
 
-public class Ingredient {
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
 
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class Ingredient {
+    private final String type;
+    private final Map<String,Object> properties = new HashMap<>();
+
+    public Ingredient(String type) {
+        this.type = type;
+    }
+
+    protected void setProperty(String key, Object value) {
+        properties.put(key, value);
+    }
+
+    protected Object getProperty(String key) {
+        return properties.get(key);
+    }
+
+    @JsonAnyGetter
+    protected Map<String,Object> getProperties() {
+        Map<String,Object> wrappedProperties = new HashMap<>();
+        wrappedProperties.put(type, properties);
+        return wrappedProperties;
+    }
 }
