e2c309bc24e4c29e6eeb53219182e6e211466660
Derek Cormier <derek.cormier@allocadia.com>
2018-01-09 15:34:57 -0800

    Automatically add compile or test sources to the maven project when the generated maven plugin is run during the generate-sources or generate-test-sources phases respectively

--- recipe-generator-maven-plugin/src/main/java/ca/derekcormier/recipe/generator/RecipeGeneratorMojo.java
+++ recipe-generator-maven-plugin/src/main/java/ca/derekcormier/recipe/generator/RecipeGeneratorMojo.java
@@ -1,17 +1,25 @@
 package ca.derekcormier.recipe.generator;
 
 import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecution;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Mojo(name = "generate", defaultPhase = LifecyclePhase.GENERATE_TEST_RESOURCES)
 public class RecipeGeneratorMojo extends AbstractMojo {
+    @Component
+    private MojoExecution execution;
+    @Component
+    private MavenProject project;
+
     @Parameter
     private String flavour;
     @Parameter(defaultValue = "${project.basedir}/cookbook.yaml")
@@ -42,5 +50,12 @@ public class RecipeGeneratorMojo extends AbstractMojo {
         }
 
         Main.main(args.toArray(new String[args.size()]));
+
+        if (execution.getLifecyclePhase().equals("generate-sources")) {
+            project.addCompileSourceRoot(targetDir);
+        }
+        else if (execution.getLifecyclePhase().equals("generate-test-sources")) {
+            project.addTestCompileSourceRoot(targetDir);
+        }
     }
 }
