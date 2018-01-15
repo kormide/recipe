3f2729e35289029bbb32d3f7e101ff99f80a3f92
Derek Cormier <derek.cormier@allocadia.com>
2018-01-15 09:58:33 -0800

    Return defaults for optionals when they are missing

--- recipe-java-runtime/src/main/java/ca/derekcormier/recipe/BaseIngredient.java
+++ recipe-java-runtime/src/main/java/ca/derekcormier/recipe/BaseIngredient.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonAnySetter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
+import java.lang.reflect.Array;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -44,6 +45,32 @@ public abstract class BaseIngredient {
 
     protected <T> T getProperty(String key) {
         return (T)properties.get(key);
+    }
+
+    protected <T> T getProperty(Class<T> clazz, String key) {
+        if (hasProperty(key)) {
+            return (T)getProperty(key);
+        }
+        else if (clazz == int.class) {
+            return (T)new Integer(0);
+        }
+        else if (clazz == float.class) {
+            return (T)new Float(0.0f);
+        }
+        else if (clazz == boolean.class) {
+            return (T)new Boolean(false);
+        }
+        else if (clazz == String.class) {
+            return null;
+        }
+        else if (clazz.isEnum()) {
+            return clazz.getEnumConstants()[0];
+        }
+        else if (clazz.isArray()) {
+            return (T)Array.newInstance(clazz.getComponentType(), 0);
+        }
+
+        return null;
     }
 
     protected boolean hasProperty(String key) {
