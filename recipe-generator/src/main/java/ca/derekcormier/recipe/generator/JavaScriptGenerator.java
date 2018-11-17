2b2b405fe32c19bf531c0ccfa5b542ca15fafd20
Derek Cormier <derek.m.cormier@gmail.com>
2018-11-16 17:25:41 -0800

    Apply liqp filters to template instead of globally

--- recipe-generator/src/main/java/ca/derekcormier/recipe/generator/JavaScriptGenerator.java
+++ recipe-generator/src/main/java/ca/derekcormier/recipe/generator/JavaScriptGenerator.java
@@ -1,5 +1,8 @@
 package ca.derekcormier.recipe.generator;
 
+import java.util.Arrays;
+import java.util.List;
+
 import ca.derekcormier.recipe.cookbook.Cookbook;
 import ca.derekcormier.recipe.generator.filter.JsParamFilter;
 import ca.derekcormier.recipe.generator.filter.JsValueFilter;
@@ -9,9 +12,16 @@ import liqp.filters.Filter;
 public abstract class JavaScriptGenerator extends Generator {
     public JavaScriptGenerator(Cookbook cookbook) {
         super(cookbook);
+    }
+
+    @Override
+    protected List<Filter> getTemplateFilters() {
         Filter identifierFilter = new TsIdentifierFilter();
-        Filter.registerFilter(new JsParamFilter(cookbook, identifierFilter));
-        Filter.registerFilter(new JsValueFilter(cookbook));
-        Filter.registerFilter(identifierFilter);
+
+        return Arrays.asList(
+            identifierFilter,
+            new JsParamFilter(getCookbook(), identifierFilter),
+            new JsValueFilter(getCookbook())
+        );
     }
 }
