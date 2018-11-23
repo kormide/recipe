dcc37c606595cc425093a8cefaca9feb6a122860
Derek Cormier <derek.m.cormier@gmail.com>
2018-11-22 17:40:25 -0800

    Beautify generated java code

--- recipe-generator/src/main/java/ca/derekcormier/recipe/generator/JavaGenerator.java
+++ recipe-generator/src/main/java/ca/derekcormier/recipe/generator/JavaGenerator.java
@@ -1,7 +1,12 @@
 package ca.derekcormier.recipe.generator;
 
+import com.google.googlejavaformat.java.Formatter;
+import com.google.googlejavaformat.java.FormatterException;
+import com.google.googlejavaformat.java.JavaFormatterOptions;
+
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 import ca.derekcormier.recipe.cookbook.Cookbook;
 import ca.derekcormier.recipe.generator.filter.JavaGetterFilter;
@@ -28,5 +33,19 @@ public abstract class JavaGenerator extends Generator {
             new JavaValueFilter(getCookbook()),
             new JavaGetterFilter()
         );
+    }
+
+    @Override
+    protected String renderTemplate(String templatePath, Map<String, Object> data) {
+        String generatedJava = super.renderTemplate(templatePath, data);
+
+        // make the generated java pretty
+        Formatter formatter = new Formatter(JavaFormatterOptions.builder().style(JavaFormatterOptions.Style.AOSP).build());
+        try {
+            return formatter.formatSource(generatedJava);
+        }
+        catch (FormatterException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
